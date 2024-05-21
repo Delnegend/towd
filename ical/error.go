f10eb10f81989048f7dacc4fe5dae89caa8473d4
Delnegend <kiennguyen19323@gmail.com>
2024-05-21 10:54:41 +0700

    feat(ical): :sparkles: error msg

---
+++ ical/error.go
@@ -0,0 +1,12 @@
+package ical
+
+var (
+	ErrIDNotInit        = "id not initialized"
+	ErrSummaryNotSet    = "summary not set"
+	ErrStartDateInvalid = "start date not set"
+	ErrEndDateInvalid   = "end date not set"
+	ErrOrganizerNotSet  = "organizer not set"
+
+	ErrInvalidURL            = "invalid url"
+	ErrStartDateAfterEndDate = "start date is after end date"
+)
