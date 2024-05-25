5429ecffc4941a7e02120fd430b974fb67942724
Delnegend <kiennguyen19323@gmail.com>
2024-05-25 07:29:53 +0700

    feat(ical/error.go): :sparkles: rm some unused errors; adding new ones

--- src-server/ical/error.go
+++ src-server/ical/error.go
@@ -6,43 +6,19 @@ import (
 )
 
 var (
-	errIDNotInit        = "id not initialized"
-	errSummaryNotSet    = "summary not set"
-	errStartDateInvalid = "start date not set"
-	errEndDateInvalid   = "end date not set"
-	errOrganizerNotSet  = "organizer not set"
-
-	errInvalidURL            = "invalid url"
-	errStartDateAfterEndDate = "start date is after end date"
+	errEventNotRecursive = "this event is not recurring"
 )
 
-func parseSlogErr(msg string, lineCount int, content string) *utils.SlogError {
+func errNestedBlock(blockName string, lineCount int, content string) *utils.SlogError {
 	return &utils.SlogError{
-		Msg:   msg,
-		Props: []interface{}{"line", lineCount, "content", content},
+		Msg:  fmt.Sprintf("nested %s block", blockName),
+		Args: []interface{}{"line", lineCount, "content", content},
 	}
 }
 
-func errNestedBlock(blockName string, lineCount int, content string) *utils.SlogError {
-	return parseSlogErr(fmt.Sprintf("nested %s block", blockName), lineCount, content)
-}
-
-func errAlarmNotInEvent(lineCount int, content string) *utils.SlogError {
-	return parseSlogErr("VALARM block not in VEVENT block", lineCount, content)
-}
-
-func errStandardNotInTimezone(lineCount int, content string) *utils.SlogError {
-	return parseSlogErr("STANDARD block not in VTIMEZONE block", lineCount, content)
-}
-
-func errExpectBegin(lineCount int, content string) *utils.SlogError {
-	return parseSlogErr("expecting BEGIN block", lineCount, content)
-}
-
 func errUnexpectedEnd(lineCount int, content string) *utils.SlogError {
-	return parseSlogErr("unexpected END block", lineCount, content)
-}
-
-func errWrongDTFormat(lineCount int, content string) *utils.SlogError {
-	return parseSlogErr("wrong DT field format", lineCount, content)
+	return &utils.SlogError{
+		Msg:  "unexpected END block",
+		Args: []interface{}{"line", lineCount, "content", content},
+	}
 }
