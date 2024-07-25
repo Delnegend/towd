34c1c0bf3a06ac4c706dd69a9633f68193d322ad
Delnegend <kiennguyen19323@gmail.com>
2024-07-26 01:25:34 +0700

    refactor(go/handler/ping.go): use embed

--- src-server/handler/ping.go
+++ src-server/handler/ping.go
@@ -2,32 +2,54 @@ package handler
 
 import (
 	"fmt"
+	"log/slog"
 	"runtime"
 	"towd/src-server/utils"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-func Ping(appState *utils.AppState) {
-	appState.AppCmdHandler["ping"] = pingHandler()
-	appState.AppCmdInfo["ping"] = &discordgo.ApplicationCommand{
-		Name:        "ping",
+func Ping(as *utils.AppState) {
+	id := "ping"
+	as.AddAppCmdHandler(id, pingHandler())
+	as.AddAppCmdInfo(id, &discordgo.ApplicationCommand{
+		Name:        id,
 		Description: "A ping command.",
-	}
+	})
 }
 
-func pingHandler() func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	return func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+func pingHandler() func(s *discordgo.Session, i *discordgo.InteractionCreate) error {
+	return func(s *discordgo.Session, i *discordgo.InteractionCreate) error {
 		latency := fmt.Sprintf("%.2fms", float64(s.HeartbeatLatency().Nanoseconds())/1000000)
 
-		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		if err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{
 				Flags: discordgo.MessageFlagsEphemeral,
-				Content: "- Server ID: `" + i.GuildID + "`" +
-					"\n- Latency: `" + latency + "`" +
-					"\n- Go version: `" + runtime.Version()[2:] + "`",
+				Embeds: []*discordgo.MessageEmbed{
+					{
+						Title: "Pong!",
+						Footer: &discordgo.MessageEmbedFooter{
+							Text: i.GuildID,
+						},
+						Fields: []*discordgo.MessageEmbedField{
+							{
+								Name:   "Latency",
+								Value:  latency,
+								Inline: true,
+							},
+							{
+								Name:   "Go version",
+								Value:  runtime.Version()[2:],
+								Inline: true,
+							},
+						},
+					},
+				},
 			},
-		})
+		}); err != nil {
+			slog.Error("can't respond", "handler", "ping", "err", err)
+		}
+		return nil
 	}
 }
