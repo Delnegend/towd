6b742cab68a69cc01fc727870259111b208b3068
Delnegend <kiennguyen19323@gmail.com>
2024-07-26 01:26:44 +0700

    feat(go/model): create_schema.go

---
+++ src-server/model/create_schema.go
@@ -0,0 +1,33 @@
+package model
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/uptrace/bun"
+)
+
+func CreateSchema(db *bun.DB) error {
+	if err := db.RunInTx(context.Background(), &sql.TxOptions{}, func(ctx context.Context, tx bun.Tx) error {
+		for _, model := range []interface{}{
+			(*Calendar)(nil),
+			(*MasterEvent)(nil),
+			(*ChildEvent)(nil),
+			(*Attendee)(nil),
+			(*RRule)(nil),
+		} {
+			if _, err := tx.
+				NewCreateTable().
+				Model(model).
+				Exec(context.Background()); err != nil {
+				return err
+			}
+		}
+		return nil
+	}); err != nil {
+		return fmt.Errorf("CreateSchema: %w", err)
+	}
+
+	return nil
+}
