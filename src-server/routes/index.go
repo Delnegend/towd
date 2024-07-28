1518e9cfd8b60d6384d2a44c1e11cfcf8eaf344f
Delnegend <kiennguyen19323@gmail.com>
2024-07-28 21:41:38 +0700

    feat(go/routes): `index` serve the SPA

---
+++ src-server/routes/index.go
@@ -0,0 +1,47 @@
+package routes
+
+import (
+	"log/slog"
+	"net/http"
+	"os"
+	"path/filepath"
+	"syscall"
+	"towd/src-server/utils"
+)
+
+func Index(as *utils.AppState, sc chan<- os.Signal) (string, func(http.ResponseWriter, *http.Request)) {
+	files := http.FS(os.DirFS(as.Config.GetStaticWebClientDir()))
+	indexFile, err := files.Open("index.html")
+	if err != nil {
+		slog.Error("Can't open index.html", "err", err)
+		sc <- syscall.SIGTERM
+	}
+	indexFileStat, err := indexFile.Stat()
+	if err != nil {
+		slog.Error("Can't get index.html stat", "err", err)
+		sc <- syscall.SIGTERM
+	}
+
+	return "GET /{filepath...}", func(w http.ResponseWriter, r *http.Request) {
+		filepath := filepath.Clean(r.PathValue("filepath"))
+		if filepath == "." {
+			filepath = "index.html"
+		}
+
+		// serve index.html if filepath not found
+		file, err := files.Open(filepath)
+		if err != nil {
+			http.ServeContent(w, r, indexFileStat.Name(), indexFileStat.ModTime(), indexFile)
+			return
+		}
+
+		// serve index.html if can't get file stat
+		stat, err := file.Stat()
+		if err != nil {
+			http.ServeContent(w, r, indexFileStat.Name(), indexFileStat.ModTime(), indexFile)
+			return
+		}
+
+		http.ServeContent(w, r, stat.Name(), stat.ModTime(), file)
+	}
+}
