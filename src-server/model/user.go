d4847d22f1c0b666619b4eb9fab6824259bfd317
Delnegend <kiennguyen19323@gmail.com>
2024-07-28 21:40:29 +0700

    feat(go/model): `user.go`

---
+++ src-server/model/user.go
@@ -0,0 +1,30 @@
+package model
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/uptrace/bun"
+)
+
+type User struct {
+	bun.BaseModel `bun:"table:users"`
+
+	ID         string `bun:"id,pk,notnull"`
+	TotpSecret string `json:"totp_secret"`
+}
+
+func (u *User) Upsert(ctx context.Context, db bun.IDB) error {
+	if u.ID == "" {
+		return fmt.Errorf("user id is empty")
+	}
+
+	_, err := db.
+		NewInsert().
+		Model(u).
+		On("CONFLICT (id) DO UPDATE").
+		Set("totp_secret = EXCLUDED.totp_secret").
+		Exec(ctx)
+
+	return err
+}
