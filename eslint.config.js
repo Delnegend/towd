73e8964e1b6ef188972a25343e2070ccb7d5eda2
Delnegend <kiennguyen19323@gmail.com>
2024-09-03 17:55:40 +0700

    chore: eslint config file

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import hagemanto from "eslint-plugin-hagemanto";
+import tailwind from "eslint-plugin-tailwindcss";
+import vue from "eslint-plugin-vue";
+import globals from "globals";
+
+export default [
+    { files: ["src/*.{ts,vue"] },
+    { ignores: ["src/components/ui/*"] },
+
+    ...hagemanto(),
+    ...tailwind.configs["flat/recommended"],
+    ...vue.configs["flat/recommended"],
+
+    {
+        rules: {
+            "tailwindcss/no-custom-classname": "off",
+            "vue/html-indent": ["error", "tab"],
+            "vue/multi-word-component-names": "off",
+            "vue/no-unused-vars": ["error", {
+                "ignorePattern": "^_"
+            }]
+        }
+    },
+    {
+        languageOptions: {
+            globals: globals.browser, parserOptions: {
+                project: true, parser: "@typescript-eslint/parser", extraFileExtensions: [".vue"]
+            }
+        }
+    }
+]
