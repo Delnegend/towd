3be21316238acf95c59430492751b03a236b40d1
Delnegend <kiennguyen19323@gmail.com>
2024-09-03 17:52:42 +0700

    refactor(`model/session_token.go`): add `// required` comments

--- src-server/model/session_token.go
+++ src-server/model/session_token.go
@@ -5,9 +5,9 @@ import "github.com/uptrace/bun"
 type SessionToken struct {
 	bun.BaseModel `bun:"table:session_tokens"`
 
-	Secret        string `bun:"secret,pk,notnull,unique"`
-	UserID        string `bun:"user_id,notnull"`
-	CreatedAtUnix int64  `bun:"created_at,notnull"`
-	IpAddress     string `bun:"ip_address,notnull"`
+	Secret        string `bun:"secret,pk"`          // required
+	UserID        string `bun:"user_id,notnull"`    // required
+	CreatedAtUnix int64  `bun:"created_at,notnull"` // required
+	IpAddress     string `bun:"ip_address,notnull"` // required
 	UserAgent     string `bun:"user_agent"`
 }
