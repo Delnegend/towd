b6e6fc7b8831d37d5f4f47d46f66a90973e3fa44
Delnegend <kiennguyen19323@gmail.com>
2024-09-15 22:50:49 +0700

    refactor(`routes/ical.go`): one type of event

--- src-server/routes/ical.go
+++ src-server/routes/ical.go
@@ -4,14 +4,10 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
-	"strconv"
-	"strings"
 	"towd/src-server/ical"
 	"towd/src-server/ical/event"
 	"towd/src-server/model"
 	"towd/src-server/utils"
-
-	"github.com/xyedo/rrule"
 )
 
 func Ical(muxer *http.ServeMux, as *utils.AppState) {
@@ -36,118 +32,33 @@ func Ical(muxer *http.ServeMux, as *utils.AppState) {
 		// turn into ical calendar
 		icalCalendar, err := func() (*ical.Calendar, error) {
 			icalCalendar := ical.NewCalendar()
-			masterEventModels := make([]model.MasterEvent, 0)
+			eventModels := make([]model.Event, 0)
 			if err := as.BunDB.
 				NewSelect().
-				Model(&masterEventModels).
+				Model(&eventModels).
 				Where("calendar_id = ?", calendarID).
-				Scan(r.Context(), &masterEventModels); err != nil {
+				Relation("Attendees").
+				Scan(r.Context(), &eventModels); err != nil {
 				return nil, err
 			}
-			slog.Info("found master events", "count", len(masterEventModels))
-			for _, masterEventModel := range masterEventModels {
-				undecidedIcalEvent := event.NewUndecidedEvent()
-				undecidedIcalEvent.
-					SetID(masterEventModel.ID).
-					SetSummary(masterEventModel.Summary).
-					SetDescription(masterEventModel.Description).
-					SetLocation(masterEventModel.Location).
-					SetURL(masterEventModel.URL).
-					SetStartDate(masterEventModel.StartDate).
-					SetEndDate(masterEventModel.EndDate).
-					SetOrganizer(masterEventModel.Organizer).
-					SetSequence(masterEventModel.Sequence).
-					SetRRuleSet(func() *rrule.Set {
-						if masterEventModel.RRule == "" {
-							return nil
-						}
-						rruleSet, err := rrule.StrToRRuleSet(masterEventModel.RRule)
-						if err != nil {
-							slog.Warn("can't parse rrule", "where", "routes/ical.go", "rrule", masterEventModel.RRule)
-							return nil
-						}
-						return rruleSet
-					}()).
-					SetExDate(func() []int64 {
-						if len(masterEventModel.ExDate) == 0 {
-							return []int64{}
-						}
-						dates := make([]int64, 0)
-						for _, exDate := range strings.Split(masterEventModel.ExDate, ",") {
-							parsedDate, err := strconv.ParseInt(exDate, 10, 64)
-							if err != nil {
-								slog.Warn("can't parse exdate", "where", "routes/ical.go", "exdate", exDate)
-								continue
-							}
-							dates = append(dates, parsedDate)
-						}
-						return dates
-					}()).
-					SetRDate(func() []int64 {
-						if len(masterEventModel.RDate) == 0 {
-							return []int64{}
-						}
-						dates := make([]int64, 0)
-						for _, rDate := range strings.Split(masterEventModel.RDate, ",") {
-							parsedDate, err := strconv.ParseInt(rDate, 10, 64)
-							if err != nil {
-								slog.Warn("can't parse rdate", "where", "routes/ical.go", "rdate", rDate)
-								continue
-							}
-							dates = append(dates, parsedDate)
-						}
-						return dates
-					}())
-
-				decidedEvent, err := undecidedIcalEvent.DecideEventType()
+			for _, eventModel := range eventModels {
+				undecidedEvent := event.NewUndecidedEvent()
+				undecidedEvent.
+					SetID(eventModel.ID).
+					SetSummary(eventModel.Summary).
+					SetDescription(eventModel.Description).
+					SetLocation(eventModel.Location).
+					SetURL(eventModel.URL).
+					SetStartDate(eventModel.StartDate).
+					SetEndDate(eventModel.EndDate).
+					SetOrganizer(eventModel.Organizer)
+				icalEventInter, err := undecidedEvent.DecideEventType()
 				if err != nil {
 					return nil, err
 				}
-
-				masterIcalEvent, ok := decidedEvent.(event.MasterEvent)
-				switch ok {
-				case true:
-					icalCalendar.AddMasterEvent(masterIcalEvent.GetID(), &masterIcalEvent)
-				case false:
-					slog.Warn("can't cast event to master event", "where", "routes/ical.go", "eventID", undecidedIcalEvent.GetID())
-					continue
+				if icalEvent, ok := icalEventInter.(event.MasterEvent); ok {
+					icalCalendar.AddMasterEvent(icalEvent.GetID(), &icalEvent)
 				}
-
-				childEventModels := make([]model.ChildEvent, 0)
-				if err := as.BunDB.
-					NewSelect().
-					Model(&childEventModels).
-					Where("id = ?", calendarID).
-					Scan(r.Context(), &childEventModels); err != nil {
-					return nil, err
-				}
-				for _, childEventModel := range childEventModels {
-					childEvent := event.NewUndecidedEvent()
-					childEvent.
-						SetID(childEventModel.MasterEventID).
-						SetRecurrenceID(childEventModel.RecurrenceID).
-						SetSummary(childEventModel.Summary).
-						SetDescription(childEventModel.Description).
-						SetLocation(childEventModel.Location).
-						SetURL(childEventModel.URL).
-						SetOrganizer(childEventModel.Organizer).
-						SetStartDate(childEventModel.StartDate).
-						SetEndDate(childEventModel.EndDate).
-						SetSequence(childEventModel.Sequence)
-
-					decidedEvent, err := childEvent.DecideEventType()
-					if err != nil {
-						return nil, err
-					}
-
-					if childIcalEvent, ok := decidedEvent.(event.ChildEvent); ok {
-						masterIcalEvent.AddChildEvent(&childIcalEvent)
-					} else {
-						slog.Warn("can't cast event to child event", "where", "routes/ical.go", "eventID", undecidedIcalEvent.GetID())
-						continue
-					}
-				}
-
 			}
 			return &icalCalendar, nil
 		}()
