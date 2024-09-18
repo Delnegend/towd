e05b6479a8de0c77bf2a7f9d327ddb69de34cfaf
Delnegend <kiennguyen19323@gmail.com>
2024-09-18 15:55:01 +0700

    refactor(`model/session.go`): strict type for purpose

--- src-server/model/session.go
+++ src-server/model/session.go
@@ -6,12 +6,21 @@ import (
 	"github.com/uptrace/bun"
 )
 
+type SessionModelPurposeType string
+
+const (
+	// for user to use to login
+	SESSION_MODEL_PURPOSE_TEMP = SessionModelPurposeType("temp")
+	// for the web client to keep the session
+	SESSION_MODEL_PURPOSE_SESSION = SessionModelPurposeType("session")
+)
+
 type Session struct {
 	bun.BaseModel `bun:"table:sessions"`
 
-	Secret    string    `bun:"secret,pk"`          // required
-	Type      string    `bun:"type,notnull"`       // required
-	UserID    string    `bun:"user_id,notnull"`    // required
-	ChannelID string    `bun:"channel_id,notnull"` // required
 	CreatedAt time.Time `bun:"created_at,notnull"` // required
+	Secret           string                  `bun:"secret,pk"`                    // required
+	Purpose          SessionModelPurposeType `bun:"purpose,notnull,type:varchar"` // required
+	UserID           string                  `bun:"user_id,notnull"`              // required
+	ChannelID        string                  `bun:"channel_id,notnull"`           // required
 }
