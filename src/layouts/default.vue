28815b60429db58d489af5d1ce54e1e052a35611
Delnegend <kiennguyen19323@gmail.com>
2024-09-24 23:20:13 +0700

    feat: `src/layouts/default.vue`

---
+++ src/layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<div class="sticky top-0 z-10 bg-white py-3">
+			<Tabs :default-value="useRoute().name?.toString()">
+				<TabsList class="mx-auto grid max-w-fit grid-cols-3">
+					<NuxtLink to="/">
+						<TabsTrigger
+							value="index"
+							class="w-full"
+						>
+							Authenticate
+						</TabsTrigger>
+					</NuxtLink>
+
+					<NuxtLink to="/calendar">
+						<TabsTrigger
+							value="calendar"
+							class="w-full"
+						>
+							Calendar
+						</TabsTrigger>
+					</NuxtLink>
+					<NuxtLink to="/kanban">
+						<TabsTrigger
+							value="kanban"
+							class="w-full"
+						>
+							Kanban board
+						</TabsTrigger>
+					</NuxtLink>
+				</TabsList>
+			</Tabs>
+		</div>
+
+		<slot />
+
+		<Toaster />
+	</div>
+</template>
