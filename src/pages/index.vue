5f1320021509f4b379d94f871c1b2c90b44ccdac
Delnegend <kiennguyen19323@gmail.com>
2024-09-25 01:16:54 +0700

    feat(`src/pages`): lorem ipsum

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,5 @@
-<script lang="ts" setup>
-</script>
-
 <template>
-	<CalendarUI />
+	<div>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo nesciunt earum aliquid explicabo odit porro maiores eos enim ut placeat fuga eaque quisquam, beatae exercitationem minima quidem illum a doloribus aliquam quod modi vero quibusdam! Magni, quae adipisci. Animi dignissimos vel fugiat eos voluptas sunt at quod placeat neque dicta deleniti voluptates veritatis pariatur ad, aperiam ea eius obcaecati minus voluptate aliquam. Dicta a suscipit natus earum similique tempore fuga culpa adipisci ex est. Reprehenderit, magnam. Quam delectus temporibus accusantium molestiae sint nesciunt praesentium quia tempora ab suscipit rem optio, commodi sunt veniam! Suscipit laudantium neque omnis obcaecati nesciunt? Commodi!
+	</div>
 </template>
