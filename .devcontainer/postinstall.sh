27f9d6eaf8466d9d5199847656930b27959a7a42
Delnegend <kiennguyen19323@gmail.com>
2024-10-12 02:38:11 +0700

    build(`.devcontainer`): `oh-my-zsh`

--- .devcontainer/postinstall.sh
+++ .devcontainer/postinstall.sh
@@ -3,3 +3,4 @@
 npm i -g pnpm
 pnpm config set store-dir /home/vscode/.pnpm-store
 go install go.abhg.dev/requiredfield/cmd/requiredfield@latest
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
