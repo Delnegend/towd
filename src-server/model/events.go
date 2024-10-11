f239707a3da4fcba7d16b3f9534fec791dab0aeb
Delnegend <kiennguyen19323@gmail.com>
2024-10-12 02:40:18 +0700

    feat(`model/events.go`): `DiffEvent`

--- src-server/model/events.go
+++ src-server/model/events.go
@@ -147,18 +147,95 @@ func (e *Event) ToDiscordEmbed(ctx context.Context, db bun.IDB) *discordgo.Messa
 	return embed
 }
 
+type DiffEvent struct {
+	Title       string
+	Description string
+	StartDate   string
+	EndDate     string
+	Location    string
+	URL         string
+	Attendees   string
 }
 
+func (e *Event) Diff(otherEvent *Event) DiffEvent {
+	diff := DiffEvent{}
+
+	switch newExist, oldExist := otherEvent.Summary != "", e.Summary != ""; {
+	case newExist && oldExist:
+		diff.Title = fmt.Sprintf("%s `[old value: %s]`", otherEvent.Summary, e.Summary)
+	case newExist && !oldExist:
+		diff.Title = fmt.Sprintf("%s `[old value: None]`", otherEvent.Summary)
+	case !newExist && oldExist:
+		diff.Title = fmt.Sprintf("%s `[unchanged]`", e.Summary)
 	}
 
+	switch otherExist, thisExist := otherEvent.Description != "", e.Description != ""; {
+	case otherExist && thisExist:
+		diff.Description = fmt.Sprintf("%s `[old value: %s]`", otherEvent.Description, e.Description)
+	case otherExist && !thisExist:
+		diff.Description = fmt.Sprintf("%s `[old value: None]`", otherEvent.Description)
+	case !otherExist && thisExist:
+		diff.Description = fmt.Sprintf("%s `[unchanged]`", e.Description)
 	}
 
-
+	switch newExist, oldExist := otherEvent.StartDateUnixUTC != 0, e.StartDateUnixUTC != 0; {
+	case newExist && oldExist:
+		diff.StartDate = fmt.Sprintf("<t:%d:f> `[old value: <t:%d:f>]`", otherEvent.StartDateUnixUTC, e.StartDateUnixUTC)
+	case newExist && !oldExist:
+		diff.StartDate = fmt.Sprintf("<t:%d:f> `[old value: None]`", otherEvent.StartDateUnixUTC)
+	case !newExist && oldExist:
+		diff.StartDate = fmt.Sprintf("<t:%d:f> `[unchanged]`", e.StartDateUnixUTC)
 	}
+
+	switch newExist, oldExist := otherEvent.EndDateUnixUTC != 0, e.EndDateUnixUTC != 0; {
+	case newExist && oldExist:
+		diff.EndDate = fmt.Sprintf("<t:%d:f> `[old value: <t:%d:f>]`", otherEvent.EndDateUnixUTC, e.EndDateUnixUTC)
+	case newExist && !oldExist:
+		diff.EndDate = fmt.Sprintf("<t:%d:f> `[old value: None]`", otherEvent.EndDateUnixUTC)
+	case !newExist && oldExist:
+		diff.EndDate = fmt.Sprintf("<t:%d:f> `[unchanged]`", e.EndDateUnixUTC)
 	}
 
+	switch newExist, oldExist := otherEvent.URL != "", e.URL != ""; {
+	case newExist && oldExist:
+		diff.URL = fmt.Sprintf("%s `[old value: %s]`", otherEvent.URL, e.URL)
+	case newExist && !oldExist:
+		diff.URL = fmt.Sprintf("%s `[old value: None]`", otherEvent.URL)
+	case !newExist && oldExist:
+		diff.URL = fmt.Sprintf("%s `[unchanged]`", e.URL)
+	}
 
+	switch newExist, oldExist := otherEvent.Location != "", e.Location != ""; {
+	case newExist && oldExist:
+		diff.Location = fmt.Sprintf("%s `[old value: %s]`", otherEvent.Location, e.Location)
+	case newExist && !oldExist:
+		diff.Location = fmt.Sprintf("%s `[old value: None]`", otherEvent.Location)
+	case !newExist && oldExist:
+		diff.Location = fmt.Sprintf("%s `[unchanged]`", e.Location)
+	}
+
+	oldAttendees := func() string {
+		attendees := make([]string, len(e.Attendees))
+		for i, attendee := range e.Attendees {
+			attendees[i] = attendee.Data
 		}
+		return strings.Join(attendees, ", ")
+	}()
+	newAttendees := func() string {
+		var attendees []string
+		for _, attendeeModel := range otherEvent.Attendees {
+			attendees = append(attendees, attendeeModel.Data)
+		}
+		return strings.Join(attendees, ", ")
+	}()
+	switch newExist, oldExist := newAttendees != "", oldAttendees != ""; {
+	case newExist && oldExist:
+		diff.Attendees = fmt.Sprintf("%s `[old value: %s]`", newAttendees, oldAttendees)
+	case newExist && !oldExist:
+		diff.Attendees = fmt.Sprintf("%s `[old value: None]`", newAttendees)
+	case !newExist && oldExist:
+		diff.Attendees = fmt.Sprintf("%s `[unchanged]`", oldAttendees)
 	}
 
+	return diff
 }
